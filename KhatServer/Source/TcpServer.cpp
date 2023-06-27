9bc68a1580a1e239bbb4fb18c06007a0ca78402e
Dwyriel <dwyriel333@hotmail.com>
2023-06-27 14:45:21 -0300

    Create KhatServer Project

---
+++ KhatServer/Source/TcpServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dwyriel on 6/27/23.
+//
+
+#include "TcpServer.h"
---
+++ KhatServer/Source/TcpServer.h
@@ -0,0 +1,14 @@
+//
+// Created by dwyriel on 6/27/23.
+//
+
+#ifndef KHATSERVER_TCPSERVER_H
+#define KHATSERVER_TCPSERVER_H
+
+
+class TcpServer {
+
+};
+
+
+#endif //KHATSERVER_TCPSERVER_H
