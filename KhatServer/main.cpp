f199d2ca197df5fa27f9314badda012801084f3f
Dwyriel <dwyriel333@hotmail.com>
2023-06-30 12:21:18 -0300

    Set up KtLogger's message handler

--- KhatServer/main.cpp
+++ KhatServer/main.cpp
@@ -2,9 +2,11 @@
 
 #include "Source/TcpServer/TcpServer.h"
 #include "Source/SignalHandler/SignalHandler.h"
+#include "KtLogger.h"
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+    qInstallMessageHandler(KtLogger::MessageHandler);
     QObject::connect(SignalHandler::instance(), &SignalHandler::CloseProgram, QCoreApplication::exit);
     TcpServer tcpServer;
     return QCoreApplication::exec();
