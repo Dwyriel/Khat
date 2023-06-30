0e192409b891bd9732954fc8d38665212167278c
Dwyriel <dwyriel333@hotmail.com>
2023-06-29 22:07:10 -0300

    Add KtLogger as lib & some definitions

--- SharedFiles/definitions.h
+++ SharedFiles/definitions.h
@@ -1,6 +1,14 @@
 #ifndef KHAT_DEFINITIONS_H
 #define KHAT_DEFINITIONS_H
 
+/*Shared Stuff*/
+#define LOGS_DIR_NAME "Logs"
+#define DATE_FORMAT "yyyy.MM.dd-hh:mm:ss"
+#define LOG_FILE_EXTENSION ".log"
+
+/*Server Stuff*/
 #define SERVER_DEFAULT_PORT 9070
+
+/*Client Stuff*/
 
 #endif //KHAT_DEFINITIONS_H
