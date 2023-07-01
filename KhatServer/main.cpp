a4f7e41f4a9e5768a6ee3fd1b01145e1840c8497
Dwyriel <dwyriel333@hotmail.com>
2023-07-01 16:38:07 -0300

    disable file logging for debug builds

--- KhatServer/main.cpp
+++ KhatServer/main.cpp
@@ -3,8 +3,12 @@
 #include "Source/TcpServer/TcpServer.h"
 #include "Source/SignalHandler/SignalHandler.h"
 #include "KtLogger.h"
+#include "cmake_config.h"
 
 int main(int argc, char *argv[]) {
+#if DEBUG_BUILD
+    KtLogger::saveToFile = false;
+#endif
     QCoreApplication a(argc, argv);
     qInstallMessageHandler(KtLogger::MessageHandler);
     QObject::connect(SignalHandler::instance(), &SignalHandler::CloseProgram, QCoreApplication::exit);
