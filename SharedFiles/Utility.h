7fd6657b8a40fca57b5e20f57d14beed0cfd45b3
Dwyriel <dwyriel333@hotmail.com>
2023-07-24 19:32:17 -0300

    Fix conversion func underflowing

--- SharedFiles/Utility.h
+++ SharedFiles/Utility.h
@@ -4,8 +4,13 @@
 #include <cstdint>
 #include <string>
 
+/**
+ * @attention To be used with trimmed strings only. If string might have whitespaces, trim it first.
+ */
 template<typename T>
 std::enable_if_t<std::is_integral_v<T>, T> convertToUnsignedInteger(const std::string &string, uint64_t maxValueAllowed) {
+    if(string[0] == '-')
+        return 0;
     char *lastReadChar;
     uint64_t conversion = strtoull(string.c_str(), &lastReadChar, 10);
     if (*lastReadChar != '\0')
